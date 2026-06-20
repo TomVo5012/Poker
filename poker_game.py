778cc593854eabec93b97c154d69d87d12c93285
TomVo5012 <tomvo50122002@gmail.com>
2026-06-20 17:05:54 -0500

    add 3 more python files which are deck card and player

--- poker_game.py
+++ poker_game.py
@@ -1,4 +1,20 @@
-class Deck:
-    def __init__(self):
-        pass
+age = True
+game_is_on = True
+
+def age_verification(age):
+    if age < 18:
+        return "You are not old enough to gamble!!!!!"
+    else:
+        return "Welcome to Poker Game!"
+    
+while(game_is_on):
+    print("""
+      My name is Kiet Vo
+      I am a founder of this Poker Game App
+      Welcome to Poker Game 
+      """)
+    username = input("Please Enter Your Name: ")
+    user_age = input("Please Verify Your Age: ")
+    if (age_verification(user_age)):
+        break
     
