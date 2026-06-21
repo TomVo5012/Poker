7680f1c1203e06755660e6b30e151f0d9ea510ba
TomVo5012 <tomvo50122002@gmail.com>
2026-06-21 11:09:05 -0500

    Update: list of cards in card.py

---
+++ card.py
@@ -0,0 +1,7 @@
+class Card:
+    def __init__(self):
+        self.number = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+        self.suit = ['♠', '♥', '♦', '♣']
+    
+    def combine_card(self):
+        pass
